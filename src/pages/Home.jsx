c66edad0550d655b8b90e2d1064a81aa18b37a6c
Elisa Flemer <elisaflemer@gmail.com>
2023-05-31 18:45:20 -0300

    NavBar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import NavBar from "../components/NavBar";
+
+export default function Home() {
+    return (
+        <div className="mx-5 mt-5">
+            <NavBar/>
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "montserrat": ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        "grey": "#676767"
+      }
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("daisyui")],
+};
