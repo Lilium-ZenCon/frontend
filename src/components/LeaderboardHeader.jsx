77c12fd6981eabd600f49886c4e4c6aa6e3090df
Elisa Flemer <elisaflemer@gmail.com>
2023-06-01 22:25:01 -0300

    Leaderboard Header

--- src/components/LeaderboardHeader.jsx
+++ src/components/LeaderboardHeader.jsx
@@ -1,31 +1,20 @@
 export default function LeaderboardHeader() {
-    return (
-        <div class="navbar bg-base-100">
-  <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div class="flex-none gap-2">
-    <div class="form-control">
-      <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
-    </div>
-    <div class="dropdown dropdown-end">
-      <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-        <div class="w-10 rounded-full">
-          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+  return (
+    <div className="navbar bg-darkgreen text-white">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-3xl">All companies</a>
+      </div>
+      <div className="flex gap-6">
+        <div className="form-control flex flex-row gap-4">
+          <input
+            type="text"
+            className="input input-bordered w-64 h-8 rounded-full text-grey"
+          />
+          <button className="rounded-full bg-lightgreen px-7">
+            Search
+          </button>
         </div>
-      </label>
-      <ul tabindex="0" class="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-        <li>
-          <a class="justify-between">
-            Profile
-            <span class="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
+      </div>
     </div>
-  </div>
-</div>
-    )
+  );
 }
