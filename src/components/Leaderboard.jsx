67f864b1b06cad2a4055477a6d91c2e2154ccae5
Elisa Flemer <elisaflemer@gmail.com>
2023-06-01 23:23:14 -0300

    Completed Home

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -4,8 +4,8 @@ import Podium from "./Podium";
 
 export default function Leaderboard() {
     return (
-        <div className="min-h-screen bg-darkgreen px-12 pt-10">
-            <h1 className="text-6xl font-bold text-white leading-[65px] mb-10">Leaderboard</h1>
+        <div className="min-h-screen bg-darkgreen px-12 py-10">
+            <h1 className="text-6xl font-bold text-white leading-[65px] mb-10 mt-5">Leaderboard</h1>
             <p className="mt-2 text-white w-[28rem] text-md">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed facilisis diam. Praesent tincidunt lobortis turpis.</p>
             <div className="flex justify-between my-8">
                 <Podium place={'1st'} company={"Gerdau"} tokens={27000}/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Features from "../components/Features";
 import Hero from "../components/Hero";
 import Leaderboard from "../components/Leaderboard";
 import NavBar from "../components/NavBar";
@@ -8,6 +9,7 @@ export default function Home() {
             <NavBar/>
             <Hero/>
             <Leaderboard/>
+            <Features/>
         </div>
     )
 }
