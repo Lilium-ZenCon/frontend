c604a63043818b11e2bff42c78288108bfb7348d
Elisa Flemer <elisaflemer@gmail.com>
2023-06-07 14:12:37 -0300

    starting leaderboard

--- components/Leaderboard.jsx
+++ components/Leaderboard.jsx
@@ -1,9 +1,22 @@
 const Leaderboard = () => {
   return (
     <div className="bg-darkgreen w-full h-screen">
-        Leaderboard
+      <div className="pt-20 mx-10 text-white w-[40%]">
+        <h1 className="text-6xl font-bold">
+          Leaderboard
+        </h1>
+        <p className="text-md mt-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          sed facilisis diam. Praesent tincidunt lobortis turpis. In vehicula
+          posuere iaculis. Nunc a metus eu turpis ultrices tincidunt sed sed
+          metus.
+        </p>
+      </div>
+      <div>
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Leaderboard
+export default Leaderboard;
---
+++ components/LeaderboardCard.jsx
@@ -0,0 +1,7 @@
+const LeaderboardCard = ({place, company, tokens}) => {
+  return (
+    <div>LeaderboardCard</div>
+  )
+}
+
+export default LeaderboardCard
