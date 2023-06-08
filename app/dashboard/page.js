ef27dd7822184ca52f674b8b6e11c9fc95c861b2
Elisa Flemer <elisaflemer@gmail.com>
2023-06-08 11:05:09 -0300

    Working carousel

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,22 @@
+import NFTCarousel from '@/components/NFTCarousel'
 import React from 'react'
 
 const dashboard = () => {
   return (
-    <div>
-        <div className='flex flex-row justify-around mt-[2%]'>
+    <div className='mx-10'>
+        <h1 className='text-3xl font-bold mb-2'>Dashboard</h1>
+        <h3 className="text-lg font-semibold text-grey">
+          Achievements
+        </h3>
+    <div className='h-[80%]'>
+        <div className='flex flex-row justify-around '>
+            <NFTCarousel/>
+        </div>
+        <h3 className="text-lg font-semibold text-grey mt-6">
+          Activity
+        </h3>
+        <div className='bg-white shadow-lg rounded-md flex justify-between items-center'>
+            <div className='flex flex-col justify-between items-center'>
             <div className='bg-white w-96 h-32 shadow-lg rounded-md'>
                 <h5 className="text-sm font-bold text-grey mt-[6%] ml-[8%]">Total Footprint</h5>
                 <h1 className='font-bold text-darkgreen text-4xl ml-[8%]' >12,000</h1>
@@ -19,12 +32,9 @@ const dashboard = () => {
                 <h1 className='font-bold text-darkgreen text-4xl ml-[8%]' >12,000</h1>
 
             </div>
-
+            </div>
         </div>
-        <div className='bg-white w-[97%] h-96 ml-[1.5%] my-[2%] shadow-lg rounded-md'>
-            <h5 className="text-md font-bold text-grey  ml-[3%] pt-[2%]">Activity</h5>
-
-        </div>
+    </div>
     </div>
   )
 }
--- app/layout.js
+++ app/layout.js
@@ -14,13 +14,14 @@ export default function RootLayout({ children }) {
 
   const [walletAddress, setWalletAddress] = useState('')
   const [walletIsConnected, setWalletIsConnected] = useState(false)
+  const [tokenBalance, setTokenBalance] = useState(0)
 
   return (
     <html lang="en" className='scroll-smooth;'>
       <head>
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet"/>
       </head>
-      <WalletContext.Provider value={{walletAddress, setWalletAddress, walletIsConnected, setWalletIsConnected}}>
+      <WalletContext.Provider value={{walletAddress, setWalletAddress, walletIsConnected, setWalletIsConnected, tokenBalance, setTokenBalance}}>
       <body className='font-montserrat scroll-smooth'>
         <NavBar/>
         {children}
