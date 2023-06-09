30a4a20ddce0e1f0c74264408ae4be08f98879ef
Emanuele Morais <emanuele.lmorais@gmail.com>
2023-06-09 13:29:19 -0300

    pinata beginning

--- app/company/page.js
+++ app/company/page.js
@@ -2,6 +2,10 @@ import React from 'react'
 import Image from "next/image";
 import logo from "public/assets/logo.svg";
 
+// Use the api keys by providing the strings directly 
+const pinataSDK = require('@pinata/sdk');
+const pinata = new pinataSDK('30d7a19afe755717bffc', 'ea8d978a134be01628e7e0e6b2ae33aa97e6a1972e8074c26ef1e2709751c22e');
+
 const company = () => {
   return (
     <div>
@@ -29,7 +33,7 @@ const company = () => {
           </div>
           <div className='py-4 flex flex-col'>
             <label className=' text-white py-1'>Logo URI:</label>
-            <input className='file-input file-input-ghost  file-input-bordered w-72 max-w-xs text-darkgreen bg-white' type='file'></input>
+            <input className='file-input file-input-ghost  file-input-bordered w-72 max-w-xs text-darkgreen bg-white' type='file' onClick={console.log("banana")}></input>
           </div>
           <button type='submit' className='text-white focus:outline-none bg-grey font-semibold p-2 rounded-lg w-[100%] my-6 hover:bg-white hover:text-grey border-grey transition duration-300 ease-in-out'>Register</button>  
         </div>
