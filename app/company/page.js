c8a7b8d38efb2653e0310fd7fec2a8d0ee3343eb
Emanuele Morais <emanuele.lmorais@gmail.com>
2023-09-19 02:07:27 -0300

    company page

--- app/company/page.js
+++ app/company/page.js
@@ -1,71 +1,97 @@
 'use client';
-
+import React, { useState } from 'react';
 import Image from 'next/image';
 import logo from 'public/assets/swap-logo.svg';
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
-import Company from '../../abis/Company.json';
 
 const company = () => {
-    const [amount, setAmount] = useState(0);
-    const [balance, setBalance] = useState(0);
 
-    useEffect(() => {
-        const getBalance = async (provider) => {
-            const contractBalance = await provider.getBalance(
-                '0x47Eb14a6f6DB1e29e82909BA12430b2d7C830851'
-            );
-			console.log(balance)
-			//setBalance(contractBalance);
-        };
-
-        if (typeof window.ethereum !== 'undefined') {
-
-            const provider = new ethers.providers.Web3Provider(ethereum);
-			getBalance(provider);
-        }
-    });
+    const [type, setType] = useState('companies');
 
     return (
         <div>
-            <div className="">
-                <div className="bg-darkgreen w-5/12 rounded-md ml-40 pt-1 mt-16">
-                    <h1 className="font-bold text-white text-lg	m-6 pl-6">
-                        Withdraw
-                    </h1>
-                    <div className=" mt-4 flex flex-col  items-center justify-center"></div>
+            <div className='ml-48 mt-24'>
+                <div className='w-1/2 min-h-1/2 py-12 bg-darkgreen rounded-lg'>
+                    <div className='flex justify-center py-4 gap-4'>
+                        <button className='bg-lightgreen w-64 px-6 py-4 rounded-lg mt-8 hover:bg-white hover:text-black duration-300 ease-in-out' onClick={() => setType('companies')}>Add new device</button>
+                        <button className='bg-lightgreen w-64 px-6 py-4 rounded-lg mt-8 hover:bg-white hover:text-black duration-300 ease-in-out' onClick={() => setType('device')}>Add company</button>
+                    </div>
+                    {type === 'companies' && (
+                        <div>
+                            <h1 className='text-white font-bold text-2xl flex justify-center mt-8'>Add new company</h1>
+                            <div className='flex justify-around'>
+                                <div className='flex flex-col'>
+                                    <label className=" text-white py-1" >Company Name:</label>
+                                    <input className="w-72 h-8 rounded-lg px-4 focus:outline-none text-darkgreen" ></input>
 
-                    <div className="flex flex-col  items-center justify-center">
-                        <div className="bg-white w-[90%] h-24 rounded-md flex items-center justify-center gap-20">
-                            <p className=" focus:outline-none text-3xl font-bold">
-                                Amount:
-                            </p>
-                            <p
-                                className="focus:outline-none  text-3xl font-bold"
-                                placeholder="0"
-                            >
-                                {balance}
-                            </p>
+                                    <label className=" text-white py-1">Company Type:</label>
+                                    <input className="w-72 h-8 rounded-lg px-4 focus:outline-none text-darkgreen" ></input>
+
+                                    <label className=" text-white py-1">Company Owner:</label>
+                                    <input className="w-72 h-8 rounded-lg px-4 focus:outline-none text-darkgreen"  ></input>
+
+                                    <label className=" text-white py-1">Logo:</label>
+                                    <input
+                                    className="file-input file-input-ghost  file-input-bordered w-72 max-w-xs text-darkgreen bg-white"
+                                    type="file"
+                                    value={''}
+                                    placeholder="Upload a file"></input>
+                                </div>
+                                <div className='flex flex-col'>
+                                    <label className=" text-white py-1">Country:</label>
+                                    <input className="w-72 h-8 rounded-lg px-4 focus:outline-none text-darkgreen"  ></input>
+
+                                    <label className=" text-white py-1">Carbon Credits Emitted:</label>
+                                    <input type="number" className="w-72 h-8 rounded-lg px-4 focus:outline-none text-darkgreen" ></input>
+
+                                    <label className=" text-white py-1">Price:</label>
+                                    <input type="number" className="w-72 h-8 rounded-lg px-4 focus:outline-none text-darkgreen" ></input>
+
+                                </div>
+                            </div>
+                            <div className='flex justify-center'>
+                                <button className='bg-lightgreen w-64 px-6 py-4 rounded-lg mt-8 hover:bg-white hover:text-black duration-300 ease-in-out'>Add device</button>
+                            </div>
                         </div>
-                    </div>
+                    )}
+                    {type == 'device' && (
+                        <div>
+                            <h1 className='text-white font-bold text-2xl flex justify-center mt-8'>Add new device</h1>
+                            <div className='flex justify-around'>
+                                <div className='flex flex-col'>
+                                    <label className=" text-white py-1" >Company Name:</label>
+                                    <input className="w-72 h-8 rounded-lg px-4 focus:outline-none text-darkgreen" ></input>
 
-                    <div className="flex flex-col items-center justify-center">
-                        <button
-                            type="submit"
-                            className="text-white focus:outline-none bg-grey font-semibold p-2 rounded-lg w-[90%] my-6 hover:bg-white hover:text-grey border-grey transition duration-300 ease-in-out"
-                        >
-                            Withdraw
-                        </button>
-                    </div>
+                                    <label className=" text-white py-1">Company Type:</label>
+                                    <input className="w-72 h-8 rounded-lg px-4 focus:outline-none text-darkgreen" ></input>
+
+                                
+                                </div>
+                                <div className='flex flex-col'>
+                                    <label className=" text-white py-1">Country:</label>
+                                    <input className="w-72 h-8 rounded-lg px-4 focus:outline-none text-darkgreen"  ></input>
+
+                                    <label className=" text-white py-1">Carbon Credits Emitted:</label>
+                                    <input type="number" className="w-72 h-8 rounded-lg px-4 focus:outline-none text-darkgreen" ></input>
+
+                                </div>
+                            </div>
+                            <div className='flex justify-center'>
+                                <button className='bg-lightgreen w-64 px-6 py-4 rounded-lg mt-8 hover:bg-white hover:text-black duration-300 ease-in-out'>Add device</button>
+                            </div>
+                        </div>
+                    )}
+                    
+
                 </div>
-                <Image
-                    className="bottom-0 right-0  -z-10 absolute"
-                    src={logo}
-                    alt="logo"
-                    width="400"
-                    height="400"
-                />
             </div>
+            <Image
+                className="bottom-0 right-0  -z-10 absolute"
+                src={logo}
+                alt="logo"
+                width="400"
+                height="400"
+            />
+
         </div>
     );
 };
