1dc5360dfc8f185abd48b16a421594aa2f738533
Elisa Flemer <elisaflemer@gmail.com>
2023-09-19 00:09:18 -0300

    feat(context): add wallet context

--- app/layout.js
+++ app/layout.js
@@ -2,25 +2,22 @@
 
 import NavBar from '@/components/NavBar';
 import './globals.css';
-import WalletContext from '@/context/WalletContext';
+import { MetaMaskProvider } from '../contexts/WalletContext';
 import { useState } from 'react';
 
 export default function RootLayout({ children }) {
-	const [walletAddress, setWalletAddress] = useState('');
-	const [walletIsConnected, setWalletIsConnected] = useState(false);
-	const [tokenBalance, setTokenBalance] = useState(0);
 
 	return (
 		<html lang="en" className="scroll-smooth;">
 			<head>
 				<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet" />
 			</head>
-			<WalletContext.Provider value={{ walletAddress, setWalletAddress, walletIsConnected, setWalletIsConnected, tokenBalance, setTokenBalance }}>
+			<MetaMaskProvider>
 				<body className="font-montserrat scroll-smooth">
 					<NavBar />
 					{children}
 				</body>
-			</WalletContext.Provider>
+			</MetaMaskProvider>
 		</html>
 	);
 }
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,49 +1,88 @@
 'use client';
-
 import Image from 'next/image';
 import logo from '../public/assets/icon.svg';
-import WalletContext from '@/context/WalletContext';
-import { useContext, useState } from 'react';
-import ConnectWallet from './ConnectWallet';
+import { useState } from 'react';
 import Link from 'next/link';
+import { useMetaMask } from '../contexts/WalletContext';
 
 const NavBar = () => {
-	const { walletAddress, setWalletAddress, walletIsConnected, setWalletIsConnected } = useContext(WalletContext);
+    const { account, connectMetaMask } = useMetaMask();
 
-	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-	const [isDropdownOpenAdmin, setIsDropdownOpenAdmin] = useState(false);
-	const [isDropdownOpenCompany, setIsDropdownOpenCompany] = useState(false); 
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-	return (
-		<div className="flex justify-between font-semibold text-grey items-center my-6 mx-10 font-montserrat">
-			<span className="flex justify-between items-center gap-14">
-				<Image src={logo} alt="logo" width="50" height="50" />
-				<li className="list-none gap-14 flex items-center scroll-smooth	">
-					<Link className="hover:bg-hover_grey rounded-lg py-2 px-4 transition duration-300 ease-in-out" href="/#hero">
-						Home
-					</Link>
-					<Link className="hover:bg-hover_grey rounded-lg py-2 px-4 transition duration-300 ease-in-out" href="/#leaderboard">
-						Leaderboard
-					</Link>
-					<Link className="hover:bg-hover_grey rounded-lg py-2 px-4 transition duration-300 ease-in-out" href="/#how-it-works">
-						How it works
-					</Link>
-				</li>
-			</span>
-			<ConnectWallet
-				walletAddress={walletAddress}
-				setWalletAddress={setWalletAddress}
-				walletIsConnected={walletIsConnected}
-				setWalletIsConnected={setWalletIsConnected}
-				setIsDropdownOpen={setIsDropdownOpen}
-				isDropdownOpen={isDropdownOpen}
-				setIsDropdownOpenAdmin = {setIsDropdownOpenAdmin}
-				isDropdownOpenAdmin = {isDropdownOpenAdmin}
-				setIsDropdownOpenCompany = {setIsDropdownOpenCompany}
-				isDropdownOpenCompany = {isDropdownOpenCompany}
-			/>
-		</div>
-	);
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    return (
+        <div className="flex justify-between font-semibold text-grey items-center my-6 mx-10 font-montserrat">
+            <span className="flex justify-between items-center gap-14">
+                <Image src={logo} alt="logo" width="50" height="50" />
+                <li className="list-none gap-14 flex items-center scroll-smooth	">
+                    <Link
+                        className="hover:bg-hover_grey rounded-lg py-2 px-4 transition duration-300 ease-in-out"
+                        href="/#hero"
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        className="hover:bg-hover_grey rounded-lg py-2 px-4 transition duration-300 ease-in-out"
+                        href="/#leaderboard"
+                    >
+                        Leaderboard
+                    </Link>
+					<Link
+                        className="hover:bg-hover_grey rounded-lg py-2 px-4 transition duration-300 ease-in-out"
+                        href="/#how-it-works"
+                    >
+                        Add company
+                    </Link>
+                    <Link
+                        className="hover:bg-hover_grey rounded-lg py-2 px-4 transition duration-300 ease-in-out"
+                        href="/#how-it-works"
+                    >
+                        IoT Simulation
+                    </Link>
+                </li>
+            </span>
+            <div className="relative">
+                {account ? (
+                    <>
+                        <button
+                            type="button"
+                            className="hover:bg-hover_grey px-4 py-2 rounded-full border-[1px] border-grey transition duration-300 ease-in-out flex"
+                            onClick={toggleDropdown}
+                        >
+                            {account.substring(0, 6) + '...' + account.substring(38, 42)}
+							<div className="flex items-center ml-2">
+                            <span>{123} ETH</span>
+                            {/* Add an icon for representing the balance */}
+                            <img src="/path-to-your-icon.png" alt="Balance Icon" width="24" height="24" />
+                        </div>
+                        </button>
+                        {isDropdownOpen && (
+                            <div className="absolute right-0 mt-2 bg-white border border-gray-300 rounded-lg shadow-lg w-full p-2">
+                                {/* Add your dropdown content and links here */}
+                                <ul>
+                                    <li>
+                                        <Link href="/">Withdraw</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        )}
+                    </>
+                ) : (
+                    <button
+                        type="button"
+                        className="hover:bg-hover_grey px-4 py-2 rounded-full border-[1px] border-grey transition duration-300 ease-in-out"
+                        onClick={connectMetaMask}
+                    >
+                        Connect wallet
+                    </button>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default NavBar;
